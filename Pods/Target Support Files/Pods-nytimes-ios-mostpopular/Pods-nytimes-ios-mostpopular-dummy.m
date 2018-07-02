86ae5188e628f246a0ac8770d0dc1f7a7759377c
Jerald Allen Abille <thejeraldo@gmail.com>
2018-07-02 23:35:04 +0400

    Add and install pods: Alamofire and SVProgressHUD

---
+++ Pods/Target Support Files/Pods-nytimes-ios-mostpopular/Pods-nytimes-ios-mostpopular-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nytimes_ios_mostpopular : NSObject
+@end
+@implementation PodsDummy_Pods_nytimes_ios_mostpopular
+@end
---
+++ Pods/Target Support Files/Pods-nytimes-ios-mostpopularUITests/Pods-nytimes-ios-mostpopularUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nytimes_ios_mostpopularUITests : NSObject
+@end
+@implementation PodsDummy_Pods_nytimes_ios_mostpopularUITests
+@end
