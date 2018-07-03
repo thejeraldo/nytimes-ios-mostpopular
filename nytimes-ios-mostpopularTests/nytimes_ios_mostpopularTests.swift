9afe92282d9a3ce9808669aaa5a7e4f23fe9e281
Jerald Allen Abille <thejeraldo@gmail.com>
2018-07-03 06:45:37 +0400

    Create WebServiceProtocol and NYTimesAPI

--- nytimes-ios-mostpopularTests/nytimes_ios_mostpopularTests.swift
+++ nytimes-ios-mostpopularTests/nytimes_ios_mostpopularTests.swift
@@ -24,19 +24,14 @@ class nytimes_ios_mostpopularTests: XCTestCase {
   func testSectionsAPI() {
     let expectations = XCTestExpectation(description: "sections api")
     
-    if let url = URL(string: baseURL)?.appendingPathComponent("svc/mostpopular/v2/viewed/sections.json") {
-      let params: Parameters = [
-        "api-key": apiKey
-      ]
-      Alamofire.request(url, method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
-        guard response.error == nil else {
-          expectations.fulfill()
-          return
-        }
-        print(response)
-        XCTAssert(response.value != nil, "response was nil")
+    Alamofire.request(NYTimesAPI.getSections()).responseJSON { (response) in
+      guard response.error == nil else {
         expectations.fulfill()
+        return
       }
+      print(response)
+      XCTAssert(response.value != nil, "response was nil")
+      expectations.fulfill()
     }
     
     wait(for: [ expectations ], timeout: 60)
@@ -45,19 +40,14 @@ class nytimes_ios_mostpopularTests: XCTestCase {
   func testMostPopulaAPI() {
     let expectations = XCTestExpectation(description: "mostpopular api")
     
-    if let url = URL(string: baseURL)?.appendingPathComponent("svc/mostpopular/v2/mostviewed/all-sections/7.json") {
-      let params: Parameters = [
-        "api-key": apiKey
-      ]
-      Alamofire.request(url, method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
-        guard response.error == nil else {
-          expectations.fulfill()
-          return
-        }
-        print(response)
-        XCTAssert(response.value != nil, "response was nil")
+    Alamofire.request(NYTimesAPI.getMostPopularForSection("all-sections", timePeriod: .week)).responseJSON { (response) in
+      guard response.error == nil else {
         expectations.fulfill()
+        return
       }
+      print(response)
+      XCTAssert(response.value != nil, "response was nil")
+      expectations.fulfill()
     }
     
     wait(for: [ expectations ], timeout: 60)
